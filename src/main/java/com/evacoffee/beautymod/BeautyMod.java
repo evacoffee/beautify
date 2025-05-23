21bb5842efd991ab60e42dc0a0de6a4151ab3104
ec <evmaagca@gmail.com>
2025-05-23 14:42:30 -0400

    Update BeautyMod.java

--- src/main/java/com/evacoffee/beautymod/BeautyMod.java
+++ src/main/java/com/evacoffee/beautymod/BeautyMod.java
@@ -4,6 +4,10 @@ import com.evacoffee.beautymod.item.ModItems;
 import com.evacoffee.beautymod.command.LoveCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import com.yourname.beautymod.events.GiftHandler;
+
+// inside onInitialize()
+GiftHandler.register();
 
 public class BeautyMod implements ModInitializer {
     @Override
