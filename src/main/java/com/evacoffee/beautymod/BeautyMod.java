1cda38ce4bc519386b7fe038df72fbf09d22b3be
ec <evmaagca@gmail.com>
2025-05-23 14:39:10 -0400

    Update BeautyMod.java

--- src/main/java/com/evacoffee/beautymod/BeautyMod.java
+++ src/main/java/com/evacoffee/beautymod/BeautyMod.java
@@ -1,12 +1,17 @@
 package com.evacoffee.beautymod;
 
-import com.yourname.beautymod.item.ModItems;
+import com.evacoffee.beautymod.item.ModItems;
+import com.evacoffee.beautymod.command.LoveCommand;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class BeautyMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
-        System.out.println("Hello from Beauty Mod!");
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            LoveCommand.register(dispatcher);
+        });
+        System.out.println("Beauty Mod initialized.");
     }
 }
