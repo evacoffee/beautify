0e038d78800b8769208cd0c05c625ddd6f15cd2c
ec <evmaagca@gmail.com>
2025-05-23 14:33:50 -0400

    Update BeautyMod.java

--- src/main/java/com/evacoffee/beautymod/BeautyMod.java
+++ src/main/java/com/evacoffee/beautymod/BeautyMod.java
@@ -1,10 +1,12 @@
-package com.yourname.beautymod;
+package com.evacoffee.beautymod;
 
+import com.yourname.beautymod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class BeautyMod implements ModInitializer {
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
         System.out.println("Hello from Beauty Mod!");
     }
 }
