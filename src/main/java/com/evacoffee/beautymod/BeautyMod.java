cb14d3a21c4da9092646d7ee408bf86bf8db0860
ec <evmaagca@gmail.com>
2025-05-23 14:30:48 -0400

    Create BeautyMod.java

---
+++ src/main/java/com/evacoffee/beautymod/BeautyMod.java
@@ -0,0 +1,10 @@
+package com.yourname.beautymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BeautyMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello from Beauty Mod!");
+    }
+}
