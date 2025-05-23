1042fc0ca60b2c2e1c1ab8b566278ffdab948e1c
ec <evmaagca@gmail.com>
2025-05-23 14:32:58 -0400

    Create ModItems.java

---
+++ src/main/java/com/evacoffee/beautymod/item/ModItems.java
@@ -0,0 +1,18 @@
+package com.evacoffee.beautymod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item LIPSTICK = registerItem("lipstick", new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier("beautymod", name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Beauty Mod Items");
+    }
+}
